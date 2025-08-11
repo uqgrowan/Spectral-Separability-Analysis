6355b39fc0bc1b8080c9cf49a97f20e007d184d8
uqgrowan <g.rowan@uq.edu.au>
2025-08-12 09:27:23 +1000

    Stats add on

--- stats/hyper_denoiser.py
+++ stats/hyper_denoiser.py
@@ -2,10 +2,11 @@ import pywt
 import numpy as np
 
 def denoise_spectrum(spectrum, wavelet='db8', threshold_mode='soft'):
+    length = len(spectrum)
     diff = np.diff(spectrum)
     sigma = np.std(diff) / np.sqrt(2)
-    coeffs = pywt.wavedec(spectrum, wavelet)
+    coeffs = pywt.wavedec(spectrum, wavelet, mode = "smooth")
     threshold = sigma * np.sqrt(2 * np.log(len(spectrum)))
     coeffs_thresh = [pywt.threshold(c, threshold, threshold_mode) for c in coeffs]
-    return pywt.waverec(coeffs_thresh, wavelet)
+    return pywt.waverec(coeffs_thresh, wavelet, mode = "smooth")[:length]
 
